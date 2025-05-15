fb966a48f302a1202483aebfefa495ab396c34fa
박주원 <qkrwndnjs7308@gmail.com>
2025-05-15 22:49:28 +0900

    feat ( #1 ) : 스프링부트 기본 세팅

---
+++ src/test/java/dsm/hs/kr/whispy_be/WhispyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dsm.hs.kr.whispy_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhispyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
