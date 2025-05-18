fc9da4ecac151d58a50c5499b71be8ec098a3d31
박주원 <qkrwndnjs7308@gmail.com>
2025-05-18 21:46:51 +0900

    feat ( #2 ) : config 설정

---
+++ Whispy-infrastructure/src/main/java/whispy_server/whispy/global/config/ConfigurationPropertiesConfig.java
@@ -0,0 +1,9 @@
+package whispy_server.whispy.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationPropertiesScan("whispy_server.whispy")
+@Configuration
+public class ConfigurationPropertiesConfig {
+}
