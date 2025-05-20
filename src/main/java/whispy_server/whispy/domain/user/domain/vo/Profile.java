12ab54cea0f73fb097bb23b60fbbacd6734332c1
박주원 <qkrwndnjs7308@gmail.com>
2025-05-20 22:38:17 +0900

    feat ( #7 ) : add User Vo

---
+++ src/main/java/whispy_server/whispy/domain/user/domain/vo/Profile.java
@@ -0,0 +1,10 @@
+package whispy_server.whispy.domain.user.domain.vo;
+
+import whispy_server.whispy.domain.user.model.types.Gender;
+
+public record Profile(
+        String name,
+        String profileImageUrl,
+        Gender gender
+) {
+}
---
+++ src/main/java/whispy_server/whispy/domain/user/domain/vo/SocialInfo.java
@@ -0,0 +1,7 @@
+package whispy_server.whispy.domain.user.domain.vo;
+
+public record SocialInfo(
+        String provider,
+        String providerId
+) {
+}
