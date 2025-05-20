397f3fd190384082f8e4b161a29fede753a3fe76
박주원 <qkrwndnjs7308@gmail.com>
2025-05-20 20:15:36 +0900

    feat ( #7 ) : add CustomDetailService

---
+++ src/main/java/whispy_server/whispy/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package whispy_server.whispy.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import whispy_server.whispy.domain.user.domain.User;
+import whispy_server.whispy.domain.user.port.in.UserFacadeUseCase;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserFacadeUseCase userFacadeUseCase;
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user = userFacadeUseCase.getUserByEmail(email);
+                return new CustomUserDetails(user.id().toString(),user.role().name());
+    }
+}
