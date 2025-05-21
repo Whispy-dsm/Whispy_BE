fa6dddc70f4c43d7e7f4271045a992d3e5807d65
박주원 <qkrwndnjs7308@gmail.com>
2025-05-21 18:50:38 +0900

    feat ( #3 ) : GlobalExceptionFilter 추가

--- src/main/java/whispy_server/whispy/global/exception/GlobalExceptionFilter.java
+++ src/main/java/whispy_server/whispy/global/exception/GlobalExceptionFilter.java
@@ -1,4 +1,45 @@
 package whispy_server.whispy.global.exception;
 
-public class GlobalExceptionFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+import whispy_server.whispy.global.exception.error.ErrorCode;
+import whispy_server.whispy.global.exception.error.ErrorResponse;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class GlobalExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws IOException {
+
+        try{
+            filterChain.doFilter(request,response);
+        }catch (WhispyException e){
+            ErrorCode errorCode = e.getErrorCode();
+            writerErrorResponse(response, errorCode.getStatusCode(), ErrorResponse.of(errorCode, errorCode.getMessage(), e));
+        } catch (Exception e){
+            e.printStackTrace();
+            writerErrorResponse(response, response.getStatus(), ErrorResponse.of(response.getStatus(),e.getMessage(), e));
+        }
+
+    }
+
+
+    private void writerErrorResponse(HttpServletResponse response, int statusCode, ErrorResponse errorResponse) throws IOException {
+        response.setStatus(statusCode);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
 }
