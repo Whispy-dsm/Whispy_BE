612839843004a723fc801319d2190d0223a50afe
박주원 <qkrwndnjs7308@gmail.com>
2025-05-21 20:01:12 +0900

    feat ( #7 ) SecurityConfig 설정

--- src/main/java/whispy_server/whispy/global/config/security/SecurityConfig.java
+++ src/main/java/whispy_server/whispy/global/config/security/SecurityConfig.java
@@ -1,4 +1,49 @@
 package whispy_server.whispy.global.config.security;
 
-public class SecurityConfig {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import whispy_server.whispy.global.config.filter.FilterConfig;
+import whispy_server.whispy.global.security.jwt.JwtTokenProvider;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {  // SecurityFilterChain 상속 제거
+
+    private final ObjectMapper objectMapper;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .headers(header -> header
+                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                                .xssProtection(HeadersConfigurer.XXssConfig::disable)
+                                .contentSecurityPolicy(csp -> csp.policyDirectives("default-src 'self'; frame-ancestors 'none';"))
+
+                )
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"))
+                        .accessDeniedHandler((request, response, accessDeniedException) ->  response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied"))
+                )
+
+                .with(new FilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults());
+
+
+                return http.build();
+
+    }
 }
