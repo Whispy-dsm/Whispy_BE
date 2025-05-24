1ec4e70121ca0792f5d3fb6f334f1f7d17583108
박주원 <qkrwndnjs7308@gmail.com>
2025-05-24 21:17:21 +0900

    feat ( #10 ) : Oauth Handler 추가

---
+++ src/main/java/whispy_server/whispy/global/oauth/handler/OauthFailureHandler.java
@@ -0,0 +1,41 @@
+package whispy_server.whispy.global.oauth.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import whispy_server.whispy.global.exception.error.ErrorCode;
+import whispy_server.whispy.global.exception.error.ErrorResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class OauthFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        ErrorCode errorCode = ErrorCode.OAUTH2_AUTHENTICATION_FAILED;
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode, request.getRequestURI(), exception);
+
+        response.setStatus(errorCode.getStatusCode());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        response.getWriter().flush();
+    }
+
+
+}
