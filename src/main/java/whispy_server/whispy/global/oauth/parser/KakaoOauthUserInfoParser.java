e5d778d3f32ddfac3be187fb9ed33a243f6e5149
박주원 <qkrwndnjs7308@gmail.com>
2025-05-24 21:16:09 +0900

    feat ( #10 ) : Oauth Parser 추가

---
+++ src/main/java/whispy_server/whispy/global/oauth/parser/KakaoOauthUserInfoParser.java
@@ -0,0 +1,19 @@
+package whispy_server.whispy.global.oauth.parser;
+
+import whispy_server.whispy.global.oauth.dto.OauthUserInfo;
+import java.util.Map;
+
+public class KakaoOauthUserInfoParser implements OauthUserInfoParser {
+
+    @Override
+    public OauthUserInfo parse(Map<String, Object> attributes) {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        return new OauthUserInfo(
+                (String) profile.get("nickname"),
+                (String) account.get("email"),
+                (String) profile.get("profile_image_url")
+        );
+    }
+}
