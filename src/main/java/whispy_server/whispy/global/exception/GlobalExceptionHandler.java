54b884cc4469d9557e6ed0033d513e03f76903ae
박주원 <qkrwndnjs7308@gmail.com>
2025-07-03 11:24:04 +0900

    feat ( #24 ) : Sentry가 에러를 수집하도록 변경 + 디스코드 알림

--- src/main/java/whispy_server/whispy/global/exception/GlobalExceptionHandler.java
+++ src/main/java/whispy_server/whispy/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package whispy_server.whispy.global.exception;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,24 +8,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import whispy_server.whispy.global.exception.error.ErrorCode;
 import whispy_server.whispy.global.exception.error.ErrorResponse;
+import whispy_server.whispy.global.webhook.DiscordNotificationService;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final DiscordNotificationService discordNotificationService;
+    private final ErrorNotificationHandler errorNotificationHandler;
 
     @ExceptionHandler(WhispyException.class)
     public ResponseEntity<ErrorResponse> handleWhispyException(WhispyException e) {
 
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode, errorCode.getMessage(), e);
-            e.printStackTrace();
-            log.error(e.toString());
 
-            return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
+        errorNotificationHandler.handleWhispyException(e);
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e){
+
+        errorNotificationHandler.handleExceptionException(e);
 
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage(), e);
