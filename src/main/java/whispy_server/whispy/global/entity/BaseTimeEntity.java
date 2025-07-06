cb2bffb38a8e7a320a2a924c4e306b112f346204
박주원 <qkrwndnjs7308@gmail.com>
2025-07-06 23:40:57 +0900

    feat ( #23 ) : baseTimeEntity 추가

---
+++ src/main/java/whispy_server/whispy/global/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package whispy_server.whispy.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import whispy_server.whispy.global.config.jpa.JpaAuditingConfig;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
