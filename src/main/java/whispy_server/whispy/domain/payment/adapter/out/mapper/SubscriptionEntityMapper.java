1ce7a6b373d62f9624da19479a40aa575bc6f6ba
박주원 <qkrwndnjs7308@gmail.com>
2025-07-06 23:44:16 +0900

    feat ( #23 ) : 매퍼 추가

---
+++ src/main/java/whispy_server/whispy/domain/payment/adapter/out/mapper/SubscriptionEntityMapper.java
@@ -0,0 +1,20 @@
+package whispy_server.whispy.domain.payment.adapter.out.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import whispy_server.whispy.domain.payment.adapter.out.entity.SubscriptionJpaEntity;
+import whispy_server.whispy.domain.payment.model.Subscription;
+
+import java.util.Optional;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface SubscriptionEntityMapper {
+
+    Subscription toModel(SubscriptionJpaEntity entity);
+    SubscriptionJpaEntity toEntity(Subscription subscription);
+
+    default Optional<Subscription> toOptionalModel(Optional<SubscriptionJpaEntity> optionalEntity){
+        return optionalEntity.map(this::toModel);
+    }
+}
