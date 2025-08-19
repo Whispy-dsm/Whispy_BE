d196f8b52bf4d734e246d8e921e78f1ecc19e2f6
박주원 <qkrwndnjs7308@gmail.com>
2025-08-19 21:23:04 +0900

    feat ( #33 ) : saveAll 메서드 추가

--- src/main/java/whispy_server/whispy/domain/fcm/application/port/out/SaveNotificationPort.java
+++ src/main/java/whispy_server/whispy/domain/fcm/application/port/out/SaveNotificationPort.java
@@ -2,6 +2,9 @@ package whispy_server.whispy.domain.fcm.application.port.out;
 
 import whispy_server.whispy.domain.fcm.model.Notification;
 
+import java.util.List;
+
 public interface SaveNotificationPort {
     Notification save(Notification notification);
+    List<Notification> saveAll(List<Notification> notifications);
 }
--- src/main/java/whispy_server/whispy/domain/fcm/application/port/out/SaveTopicSubscriptionPort.java
+++ src/main/java/whispy_server/whispy/domain/fcm/application/port/out/SaveTopicSubscriptionPort.java
@@ -2,6 +2,9 @@ package whispy_server.whispy.domain.fcm.application.port.out;
 
 import whispy_server.whispy.domain.fcm.model.TopicSubscription;
 
+import java.util.List;
+
 public interface SaveTopicSubscriptionPort {
     TopicSubscription save(TopicSubscription topicSubscription);
+    List<TopicSubscription> saveAll(List<TopicSubscription> subscriptions);
 }
