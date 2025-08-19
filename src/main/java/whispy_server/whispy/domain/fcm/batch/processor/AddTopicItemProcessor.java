df6050d286cd25998e643fdba586c9b0a0801fa2
박주원 <qkrwndnjs7308@gmail.com>
2025-08-19 21:24:13 +0900

    feat ( #33 ) : processor 추가

---
+++ src/main/java/whispy_server/whispy/domain/fcm/batch/processor/AddTopicItemProcessor.java
@@ -0,0 +1,51 @@
+package whispy_server.whispy.domain.fcm.batch.processor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+import whispy_server.whispy.domain.fcm.application.port.out.QueryTopicSubscriptionPort;
+import whispy_server.whispy.domain.fcm.batch.dto.AddTopicJobParameters;
+import whispy_server.whispy.domain.fcm.model.types.NotificationTopic;
+import whispy_server.whispy.domain.user.adapter.out.entity.UserJpaEntity;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AddTopicItemProcessor implements ItemProcessor<UserJpaEntity, AddTopicJobParameters> {
+
+    private final QueryTopicSubscriptionPort queryTopicSubscriptionPort;
+
+    private NotificationTopic newTopic;
+    private boolean defaultSubscribed;
+
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution) {
+        JobParameters jobParameters = stepExecution.getJobParameters();
+        this.newTopic = NotificationTopic.valueOf(jobParameters.getString("newTopic"));
+        this. defaultSubscribed = Boolean.parseBoolean(jobParameters.getString("defaultSubscribed"));
+    }
+
+    @Override
+    public AddTopicJobParameters process(UserJpaEntity user) throws Exception {
+        boolean alreadyExists = queryTopicSubscriptionPort
+                .findByEmailAndTopic(user.getEmail(), newTopic)
+                .isPresent();
+
+        if(alreadyExists){
+            log.debug("사용자 {}는 이미 토픽 {}을 가지고 있음", user.getEmail(), newTopic);
+            return null;
+        }
+
+        return new AddTopicJobParameters(
+                user.getEmail(),
+                user.getFcmToken(),
+                newTopic,
+                defaultSubscribed
+        );
+    }
+
+}
