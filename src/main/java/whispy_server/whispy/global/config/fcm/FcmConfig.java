2b3cb11d0958be56a95f89ac7a3e540f9a453de5
박주원 <qkrwndnjs7308@gmail.com>
2025-09-01 18:35:08 +0900

    refactor ( #33 ) : FcmConfig 수정

--- src/main/java/whispy_server/whispy/global/config/fcm/FcmConfig.java
+++ src/main/java/whispy_server/whispy/global/config/fcm/FcmConfig.java
@@ -3,6 +3,7 @@ package whispy_server.whispy.global.config.fcm;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -13,12 +14,13 @@ import java.io.IOException;
 @Configuration
 public class FcmConfig {
 
-    private static final String FCM_PATH = "firebase/whispy-app-service-d58a0439d4aa.json";
+    @Value("${fcm.fcm-path}")
+    private String fcmPath;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            ClassPathResource resource = new ClassPathResource(FCM_PATH);
+            ClassPathResource resource = new ClassPathResource(fcmPath);
             var option = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                     .build();
