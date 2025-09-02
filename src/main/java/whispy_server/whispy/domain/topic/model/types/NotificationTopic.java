9164476c01d186825001f708bd67dc6a4b00aeb9
박주원 <qkrwndnjs7308@gmail.com>
2025-09-02 19:53:55 +0900

    feat ( #33 ) : 전체 공지용, 시스템 알림 enum 추가

--- src/main/java/whispy_server/whispy/domain/topic/model/types/NotificationTopic.java
+++ src/main/java/whispy_server/whispy/domain/topic/model/types/NotificationTopic.java
@@ -2,5 +2,7 @@ package whispy_server.whispy.domain.topic.model.types;
 
 public enum NotificationTopic {
     GENERAL_ANNOUNCEMENT,
+    SYSTEM_ANNOUNCEMENT,
+    BROADCAST_ANNOUNCEMENT,
     ONLY_ADMIN
 }
