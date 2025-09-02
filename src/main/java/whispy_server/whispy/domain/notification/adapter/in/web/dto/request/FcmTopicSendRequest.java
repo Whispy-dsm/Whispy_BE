2b20fbf5e1a8545386655f2ae9f27edcd3a153fa
박주원 <qkrwndnjs7308@gmail.com>
2025-09-02 19:54:44 +0900

    feat ( #33 ) : 토픽 전송 용 dto 추가

---
+++ src/main/java/whispy_server/whispy/domain/notification/adapter/in/web/dto/request/FcmTopicSendRequest.java
@@ -0,0 +1,15 @@
+package whispy_server.whispy.domain.notification.adapter.in.web.dto.request;
+
+import whispy_server.whispy.domain.topic.model.types.NotificationTopic;
+
+import java.util.List;
+import java.util.Map;
+
+public record FcmTopicSendRequest(
+        List<String> deviceTokens,
+        NotificationTopic topic,
+        String title,
+        String body,
+        Map<String, String> data
+) {
+}
