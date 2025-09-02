c7ac55bc5c280891609a56c5354aae9f80866843
박주원 <qkrwndnjs7308@gmail.com>
2025-09-02 19:55:43 +0900

    refactor ( #33 ) : 단일 전송 시 notification 테이블에 저장하도록 변경

--- src/main/java/whispy_server/whispy/domain/notification/application/service/SendToDeviceTokensService.java
+++ src/main/java/whispy_server/whispy/domain/notification/application/service/SendToDeviceTokensService.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Service;
 import whispy_server.whispy.domain.notification.adapter.in.web.dto.request.FcmSendRequest;
 import whispy_server.whispy.domain.notification.application.port.in.SendToDeviceTokensUseCase;
 import whispy_server.whispy.domain.notification.application.port.out.FcmSendPort;
+import whispy_server.whispy.domain.notification.application.port.out.SaveNotificationPort;
+import whispy_server.whispy.domain.notification.model.Notification;
 
 @Service
 @RequiredArgsConstructor
 public class SendToDeviceTokensService implements SendToDeviceTokensUseCase {
 
     private final FcmSendPort fcmSendPort;
+    private final SaveNotificationPort saveNotificationPort;
 
     @Override
     public void execute(FcmSendRequest request){
@@ -20,5 +23,16 @@ public class SendToDeviceTokensService implements SendToDeviceTokensUseCase {
                 request.body(),
                 request.data()
         );
+
+        Notification notification = new Notification(
+                null,
+                request.email(),
+                request.title(),
+                request.body(),
+                request.topic(),
+                request.data(),
+                false
+        );
+        saveNotificationPort.save(notification);
     }
 }
