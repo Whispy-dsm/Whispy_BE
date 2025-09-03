e0938ca626ed91789cd3de285c683c4a4d30f0d8
박주원 <qkrwndnjs7308@gmail.com>
2025-09-03 22:04:13 +0900

    refactor ( #33 ) : 알림 조회 시 N+1 문제 해결

--- src/main/java/whispy_server/whispy/domain/notification/adapter/out/persistence/repository/NotificationRepository.java
+++ src/main/java/whispy_server/whispy/domain/notification/adapter/out/persistence/repository/NotificationRepository.java
@@ -1,5 +1,6 @@
 package whispy_server.whispy.domain.notification.adapter.out.persistence.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import whispy_server.whispy.domain.notification.adapter.out.entity.NotificationJpaEntity;
 
@@ -8,11 +9,11 @@ import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<NotificationJpaEntity, UUID> {
 
+    @EntityGraph(attributePaths = {"data"})
     List<NotificationJpaEntity> findByEmailOrderByCreatedAtDesc(String email);
-
+    @EntityGraph(attributePaths = {"data"})
     List<NotificationJpaEntity> findByEmailAndReadFalseOrderByCreatedAtDesc(String email);
-
+    @EntityGraph(attributePaths = {"data"})
     List<NotificationJpaEntity> findByEmailAndReadFalse(String email);
-
     int countByEmailAndReadFalse(String email);
 }
