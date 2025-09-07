e273b77b6b94e7d27095c3eb1f7943108fd20acb
박주원 <qkrwndnjs7308@gmail.com>
2025-09-07 21:18:30 +0900

    feat ( #37 ) : external 추가

---
+++ src/main/java/whispy_server/whispy/domain/auth/adapter/out/external/EmailSendAdapter.java
@@ -0,0 +1,33 @@
+package whispy_server.whispy.domain.auth.adapter.out.external;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailSendAdapter {
+
+    private final JavaMailSender mailSender;
+
+    public void sendVerificationCode(String email, String code) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject("Whispy 이메일 인증 코드");
+            message.setText(String.format(
+                            "안녕하세요!\n\n" +
+                            "Whispy 이메일 인증 코드입니다.\n\n" +
+                            "인증 코드: %s\n\n" +
+                            "이 코드는 5분 후 만료됩니다.\n" +
+                            "본인이 요청하지 않았다면 이 메일을 무시해주세요.",
+                            code
+            ));
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            throw new RuntimeException("이메일 전송에 실패했습니다.", e);
+        }
+    }
+}
