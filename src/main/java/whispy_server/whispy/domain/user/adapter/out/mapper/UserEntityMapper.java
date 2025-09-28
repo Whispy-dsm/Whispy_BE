798ccb0b4d2e6d37e3684c6c8b5e3d54e21e108e
박주원 <qkrwndnjs7308@gmail.com>
2025-09-28 17:17:10 +0900

    chore ( #43 ) : 잘못된 vo 제거

--- src/main/java/whispy_server/whispy/domain/user/adapter/out/mapper/UserEntityMapper.java
+++ src/main/java/whispy_server/whispy/domain/user/adapter/out/mapper/UserEntityMapper.java
@@ -12,12 +12,7 @@ import java.util.Optional;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserEntityMapper {
 
-    @Mapping(source = "profile.name", target = "name")
-    @Mapping(source = "profile.profileImageUrl", target = "profileImageUrl")
-    @Mapping(source = "profile.gender", target = "gender")
     UserJpaEntity toEntity(User userModel);
-
-    @Mapping(target = "profile", expression = "java(new Profile(entity.getName(), entity.getProfileImageUrl(), entity.getGender()))")
     User toModel(UserJpaEntity entity);
 
     default Optional<User> toOptionalDomain(Optional<UserJpaEntity> optionalEntity){
