e133afe340d40dd09e926d9fd04a3c34a8a8da2e
박주원 <qkrwndnjs7308@gmail.com>
2025-09-29 23:20:27 +0900

    feat ( #45 ) : 어그리게이트 추가

--- src/main/java/whispy_server/whispy/domain/announcement/model/Announcement.java
+++ src/main/java/whispy_server/whispy/domain/announcement/model/Announcement.java
@@ -1,5 +1,6 @@
 package whispy_server.whispy.domain.announcement.model;
 
+import whispy_server.whispy.domain.announcement.adapter.in.web.dto.request.UpdateAnnouncementRequest;
 import whispy_server.whispy.global.annotation.Aggregate;
 
 @Aggregate
@@ -8,4 +9,13 @@ public record Announcement(
         String title,
         String content,
         String bannerImageUrl
-) {}
+) {
+    public Announcement update(UpdateAnnouncementRequest request) {
+        return new Announcement(
+                this.id,
+                request.title() != null ? request.title() : this.title,
+                request.content() != null ? request.content() : this.content,
+                request.bannerImageUrl() != null ? request.bannerImageUrl() : this.bannerImageUrl
+        );
+    }
+}
