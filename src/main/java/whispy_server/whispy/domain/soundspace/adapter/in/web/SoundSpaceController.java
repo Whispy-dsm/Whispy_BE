4813eceb3602f5d33c1724e5299e38b944777fa5
박주원 <qkrwndnjs7308@gmail.com>
2025-10-11 18:24:00 +0900

    feat ( #51 ) : controller 추가

---
+++ src/main/java/whispy_server/whispy/domain/soundspace/adapter/in/web/SoundSpaceController.java
@@ -0,0 +1,43 @@
+package whispy_server.whispy.domain.soundspace.adapter.in.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import whispy_server.whispy.domain.soundspace.adapter.in.web.dto.response.MusicInSoundSpaceCheckResponse;
+import whispy_server.whispy.domain.soundspace.adapter.in.web.dto.response.SoundSpaceMusicResponse;
+import whispy_server.whispy.domain.soundspace.application.port.in.CheckMusicInSoundSpaceUseCase;
+import whispy_server.whispy.domain.soundspace.application.port.in.GetSoundSpaceMusicsUseCase;
+import whispy_server.whispy.domain.soundspace.application.port.in.ToggleSoundSpaceMusicUseCase;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/soundspace")
+@RequiredArgsConstructor
+public class SoundSpaceController {
+
+    private final GetSoundSpaceMusicsUseCase getSoundSpaceMusicsUseCase;
+    private final ToggleSoundSpaceMusicUseCase toggleSoundSpaceMusicUseCase;
+    private final CheckMusicInSoundSpaceUseCase checkMusicInSoundSpaceUseCase;
+
+    @GetMapping("/music")
+    public List<SoundSpaceMusicResponse> getSoundSpaceMusics() {
+        return getSoundSpaceMusicsUseCase.execute();
+    }
+
+    @PostMapping("/toggle/{musicId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void toggleSoundSpaceMusic(@PathVariable Long musicId) {
+        toggleSoundSpaceMusicUseCase.execute(musicId);
+    }
+
+    @GetMapping("/music/{musicId}/exists")
+    public MusicInSoundSpaceCheckResponse checkMusicInSoundSpace(@PathVariable Long musicId) {
+        return checkMusicInSoundSpaceUseCase.execute(musicId);
+    }
+}
