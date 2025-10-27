e4493c06ac2e6566b19acd52425250966c9fd19d
박주원 <qkrwndnjs7308@gmail.com>
2025-10-27 22:43:46 +0900

    feat ( #54 ) : FocusSessionController 추가

---
+++ src/main/java/whispy_server/whispy/domain/focussession/adapter/in/web/FocusSessionController.java
@@ -0,0 +1,27 @@
+package whispy_server.whispy.domain.focussession.adapter.in.web;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import whispy_server.whispy.domain.focussession.adapter.in.web.dto.request.SaveFocusSessionRequest;
+import whispy_server.whispy.domain.focussession.adapter.in.web.dto.response.FocusSessionResponse;
+import whispy_server.whispy.domain.focussession.application.port.in.SaveFocusSessionUseCase;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/focus-sessions")
+public class FocusSessionController {
+
+    private final SaveFocusSessionUseCase saveFocusSessionUseCase;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public FocusSessionResponse saveFocusSession(@Valid @RequestBody SaveFocusSessionRequest request) {
+        return saveFocusSessionUseCase.execute(request);
+    }
+}
