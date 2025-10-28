a5c46f209b3dea66d63082381e72e8c8cf6c90c2
박주원 <qkrwndnjs7308@gmail.com>
2025-10-28 16:28:30 +0900

    feat ( #58 ) : port/out 추가

---
+++ src/main/java/whispy_server/whispy/domain/focussession/application/port/out/FocusSessionPort.java
@@ -0,0 +1,4 @@
+package whispy_server.whispy.domain.focussession.application.port.out;
+
+public interface FocusSessionPort extends FocusSessionSavePort, QueryFocusSessionPort, DeleteFocusSessionPort {
+}
