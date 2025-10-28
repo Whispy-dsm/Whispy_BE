a6a80fec8db7233fb8b11dc4041dc5d2e3ac7e12
박주원 <qkrwndnjs7308@gmail.com>
2025-10-28 16:29:18 +0900

    feat ( #58 ) : adapter/out 추가

--- src/main/java/whispy_server/whispy/domain/focussession/adapter/out/mapper/FocusSessionMapper.java
+++ src/main/java/whispy_server/whispy/domain/focussession/adapter/out/mapper/FocusSessionMapper.java
@@ -4,8 +4,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.data.domain.Page;
 import whispy_server.whispy.domain.focussession.adapter.out.entity.FocusSessionJpaEntity;
 import whispy_server.whispy.domain.focussession.model.FocusSession;
+
+import java.util.Optional;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FocusSessionMapper {
@@ -15,4 +18,12 @@ public interface FocusSessionMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "modifiedAt", ignore = true)
     FocusSessionJpaEntity toEntity(FocusSession domain);
+
+    default Optional<FocusSession> toOptionalModel(Optional<FocusSessionJpaEntity> optionalEntity) {
+        return optionalEntity.map(this::toModel);
+    }
+
+    default Page<FocusSession> toModelPage(Page<FocusSessionJpaEntity> entityPage) {
+        return entityPage.map(this::toModel);
+    }
 }
