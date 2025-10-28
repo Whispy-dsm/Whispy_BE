075c21d022eb4b7fb454f5f966b32eb8785284d5
박주원 <qkrwndnjs7308@gmail.com>
2025-10-28 11:39:34 +0900

    feat ( #58 ) : in/web dto

---
+++ src/main/java/whispy_server/whispy/domain/statistics/adapter/in/web/dto/response/FocusStatisticsResponse.java
@@ -0,0 +1,26 @@
+package whispy_server.whispy.domain.statistics.adapter.in.web.dto.response;
+
+import whispy_server.whispy.domain.focussession.model.types.FocusTag;
+import whispy_server.whispy.domain.statistics.model.FocusSessionSummary;
+import whispy_server.whispy.domain.statistics.model.FocusStatistics;
+
+import java.util.List;
+import java.util.Map;
+
+public record FocusStatisticsResponse(
+        int totalCount,
+        int totalMinutes,
+        int todayMinutes,
+        Map<FocusTag, Integer> tagMinutes,
+        List<FocusSessionSummary> sessions
+) {
+    public static FocusStatisticsResponse from(FocusStatistics statistics) {
+        return new FocusStatisticsResponse(
+                statistics.totalCount(),
+                statistics.totalMinutes(),
+                statistics.todayMinutes(),
+                statistics.tagMinutes(),
+                statistics.sessions()
+        );
+    }
+}
