3cd93cd96aa48df5cad75e8da9945fb9e198b1c6
박주원 <qkrwndnjs7308@gmail.com>
2025-10-28 16:27:48 +0900

    feat ( #58 ) : 어노테이션 추가

--- src/main/java/whispy_server/whispy/domain/statistics/application/port/in/GetFocusStatisticsUseCase.java
+++ src/main/java/whispy_server/whispy/domain/statistics/application/port/in/GetFocusStatisticsUseCase.java
@@ -2,9 +2,11 @@ package whispy_server.whispy.domain.statistics.application.port.in;
 
 import whispy_server.whispy.domain.statistics.adapter.in.web.dto.response.FocusStatisticsResponse;
 import whispy_server.whispy.domain.statistics.model.types.PeriodType;
+import whispy_server.whispy.global.annotation.UseCase;
 
 import java.time.LocalDate;
 
+@UseCase
 public interface GetFocusStatisticsUseCase {
     FocusStatisticsResponse execute(PeriodType period, LocalDate date);
 }
