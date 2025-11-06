003bd0ff6b644d9556f38cf290e9b6b8767b3b6d
박주원 <qkrwndnjs7308@gmail.com>
2025-11-06 11:50:28 +0900

    feat : cors 설정 추가

--- src/main/java/whispy_server/whispy/global/config/web/WebConfig.java
+++ src/main/java/whispy_server/whispy/global/config/web/WebConfig.java
@@ -2,6 +2,7 @@ package whispy_server.whispy.global.config.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import whispy_server.whispy.global.file.FileProperties;
@@ -17,5 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/file/**")
                .addResourceLocations(FILE + fileProperties.uploadPath() + "/");
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
