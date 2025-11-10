538c99e60b70cc69a00781b4d83fe933f752438c
박주원 <qkrwndnjs7308@gmail.com>
2025-11-10 21:45:30 +0900

    feat ( #65 ) : jpaEntity 추가

---
+++ src/main/java/whispy_server/whispy/domain/reason/adapter/out/entity/WithdrawalReasonJpaEntity.java
@@ -0,0 +1,40 @@
+package whispy_server.whispy.domain.reason.adapter.out.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import whispy_server.whispy.domain.reason.model.types.WithdrawalReasonType;
+import whispy_server.whispy.global.entity.BaseTimeEntity;
+
+@Entity(name = "WithdrawalReasonJpaEntity")
+@Table(name = "tbl_withdrawal_reason")
+@Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WithdrawalReasonJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WithdrawalReasonType reasonType;
+
+    @Column(name = "detail_content", columnDefinition = "TEXT")
+    private String detailContent;
+}
