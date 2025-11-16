c585ad16eb50462905848283c06c0747897bc16d
박주원 <qkrwndnjs7308@gmail.com>
2025-11-17 08:15:52 +0900

    refactor ( #69 ) : 정적 팩토리 메서드로 처리하도록 수정

--- src/main/java/whispy_server/whispy/domain/announcement/adapter/in/web/dto/response/QueryAllAnnouncementResponse.java
+++ src/main/java/whispy_server/whispy/domain/announcement/adapter/in/web/dto/response/QueryAllAnnouncementResponse.java
@@ -1,9 +1,20 @@
 package whispy_server.whispy.domain.announcement.adapter.in.web.dto.response;
 
+import whispy_server.whispy.domain.announcement.model.Announcement;
+
 public record QueryAllAnnouncementResponse(
         Long id,
         String title,
         String content,
         String bannerImageUrl
 ) {
+
+    public static QueryAllAnnouncementResponse from(Announcement announcement) {
+        return new QueryAllAnnouncementResponse(
+                announcement.id(),
+                announcement.title(),
+                announcement.content(),
+                announcement.bannerImageUrl()
+        );
+    }
 }
